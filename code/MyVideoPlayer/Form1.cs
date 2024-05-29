926bc2657be3c46e703470445d78a8c7fcc7362e
MineaEdi <126817780+MineaEdi@users.noreply.github.com>
2024-05-29 11:49:23 +0300

    header + comments + documentation

--- code/MyVideoPlayer/Form1.cs
+++ code/MyVideoPlayer/Form1.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        Form1.cs                                                 *
+ *  Copyright:   (c) 2024, Bulgaru, Constantin, Minea, Zaharia            *
+ *  Description: Această clasă reprezintă clasa principală a aplicației.  *
+ *               Gestionează evenimentele și interacțiunea utilizatorului *
+ *               cu interfața grafică. Inițializează componentele vizuale *
+ *               ale interfeței și definește comportamentul acestora în   *
+ *               răspuns la acțiunile utilizatorului.                     *
+ *                                                                        *
+ **************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +26,7 @@ namespace MyVideoPlayer
 {
     public partial class FormMain : Form
     {
+        // Variables necessary for the video player to function
         private Video video;
         private string[] videoPaths;
         private string folderPath;
@@ -21,49 +34,78 @@ namespace MyVideoPlayer
         private Size formSize;
         private Size pnlSize;
 
+        /// <summary>
+        /// Constructor of the main form
+        /// </summary>
         public FormMain()
         {
             InitializeComponent();
 
+            // Allows handling keyboard events
             this.KeyPreview = true;
         }
 
+        /// <summary>
+        /// Event triggered when the form is loaded
+        /// </summary>
         private void FormMain_Load(object sender, EventArgs e)
         {
+            // Save the initial dimensions of the form and the video panel
             formSize = new Size(this.Width, this.Height);
             pnlSize = new Size(panelMainVideo.Width, panelMainVideo.Height);
         }
 
+        /// <summary>
+        /// Event triggered when the Play/Pause button is clicked
+        /// </summary>
         private void buttonPlay_Click(object sender, EventArgs e)
         {
             Functionalities.PlayPauseVideo(video, timerVideo, buttonPlay);
         }
 
+        /// <summary>
+        /// Event triggered when the Next button is clicked
+        /// </summary>
         private void buttonNext_Click(object sender, EventArgs e)
         {
             NextVideo();
         }
 
+        /// <summary>
+        /// Event triggered when the Previous button is clicked
+        /// </summary>
         private void buttonPrevious_Click(object sender, EventArgs e)
         {
             PreviousVideo();
         }
 
+        /// <summary>
+        /// Event triggered when the Fullscreen button is clicked
+        /// </summary>
         private void buttonFullscreen_Click(object sender, EventArgs e)
         {
             Functionalities.ToggleFullscreen(this, video);
         }
 
+        /// <summary>
+        /// Event triggered when the Volume button is clicked
+        /// </summary>
         private void buttonVolume_Click(object sender, EventArgs e)
         {
             Functionalities.ToggleVolumeTrackBar(trackBarVolume);
         }
 
+        /// <summary>
+        /// Event triggered when the volume TrackBar is scrolled
+        /// </summary>
         private void trackBarVolume_Scroll(object sender, EventArgs e)
         {
             Functionalities.AdjustVolume(video, trackBarVolume);
         }
 
+        /// <summary>
+        /// Event triggered when the selection in the video ListBox changes
+        /// </summary>
         private void listBoxOfVideos_SelectedIndexChanged(object sender, EventArgs e)
         {
             try
@@ -76,39 +118,61 @@ namespace MyVideoPlayer
             }
         }
 
+        /// <summary>
+        /// Event triggered when a key is pressed
+        /// </summary>
         private void FormMain_KeyDown(object sender, KeyEventArgs e)
         {
+            // exit fullscreen when ESC key is pressed
             if (e.KeyCode == Keys.Escape)
             {
                 Functionalities.ExitFullscreen(this, video, formSize, panelMainVideo, pnlSize);
             }
         }
 
+        /// <summary>
+        /// Event triggered by the timer for updating the position of the video
+        /// </summary>
         private void timerVideo_Tick(object sender, EventArgs e)
         {
             Functionalities.UpdateVideoPosition(video, labelVideoPosition);
         }
 
+        /// <summary>
+        /// Method to play the next video
+        /// </summary>
         private void NextVideo()
         {
             Functionalities.NextVideo(listBoxOfVideos, videoPaths.Length, index => listBoxOfVideos.SelectedIndex = index);
         }
 
+        /// <summary>
+        /// Method to play the previous video
+        /// </summary>
         private void PreviousVideo()
         {
             Functionalities.PreviousVideo(listBoxOfVideos, videoPaths.Length, index => listBoxOfVideos.SelectedIndex = index);
         }
 
+        /// <summary>
+        /// Event triggered when the About button is clicked
+        /// </summary>
         private void buttonAbout_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Acesta este un program ce poate reda fișiere media cu extensia .wmv", "Despre", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        /// <summary>
+        /// Event triggered when the Help button is clicked
+        /// </summary>
         private void buttonHelp_Click(object sender, EventArgs e)
         {
             Help.ShowHelp(this, "PlayerVideo.chm");
         }
 
+        /// <summary>
+        /// Event triggered when the Open Folder button is clicked
+        /// </summary>
         private void buttonOpenFolder_Click(object sender, EventArgs e)
         {
             using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
@@ -116,16 +180,19 @@ namespace MyVideoPlayer
                 if (folderDialog.ShowDialog() == DialogResult.OK)
                 {
                     folderPath = folderDialog.SelectedPath;
+                    // check if the folderPath is not empty
                     if (!string.IsNullOrEmpty(folderPath))
                     {
                         try
                         {
-                            listBoxOfVideos.Items.Clear(); // Clear the list before loading new videos
-                            ClearCurrentVideo(); // Clear the panel and reset the video
+                            listBoxOfVideos.Items.Clear(); // clear the list before loading new videos
+                            ClearCurrentVideo(); // clear the panel and reset the video
                             Functionalities.LoadVideos(listBoxOfVideos, folderPath, out videoPaths, out selectedIndex);
 
+                            // check if in the videoPaths are no videos
                             if (videoPaths.Length == 0)
                             {
+                                // show message to user in order to inform
                                 MessageBox.Show("Folderul selectat nu conține videoclipuri .wmv.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
                                 video = null;
                                 labelVideo.Text = string.Empty;
@@ -135,26 +202,32 @@ namespace MyVideoPlayer
                         }
                         catch (Exception ex)
                         {
+                            // show error message if exists
                             MessageBox.Show("A apărut o eroare la încărcarea videoclipurilor: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
                     else
                     {
+                        // show message if the path to the folder is empty
                         MessageBox.Show("Calea către folder nu poate fi goală. Vă rugăm să selectați un folder valid.", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Method to clear the current video
+        /// </summary>
         private void ClearCurrentVideo()
         {
+            // check if one video is loaded in order to clear it
             if (video != null)
             {
-                video.Stop();
-                video.Dispose();
-                video = null;
+                video.Stop(); // stop the video that is playing
+                video.Dispose(); // clear resources taken by video
+                video = null; // reset video to null
             }
-            panelMainVideo.Controls.Clear();
+            panelMainVideo.Controls.Clear(); // clear all controls in main panel
         }
     }
 }
--- code/VideoFunctions/Functionalities.cs
+++ code/VideoFunctions/Functionalities.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        Functionalities.cs                                       *
+ *  Copyright:   (c) 2024, Bulgaru, Constantin, Minea, Zaharia            *
+ *  Description: Această clasă conține funcționalitățile principale ale   *
+ *               aplicației. Include metode pentru: încărcarea            *
+ *               videoclipurilor dintr-un director specificat, redarea,   *
+ *               oprirea, navigarea între videoclipuri, modul fullscreen  *
+ *               și ajustarea volumului. Îmbunătățește modularitatea și   *
+ *               organizarea codului prin separarea funcționalităților    *
+ *               principale într-o clasă dedicată.                        *
+ *                                                                        *
+ **************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -12,47 +26,83 @@ namespace VideoFunctions
 {
     public class Functionalities
     {
+        /// <summary>
+        /// Loads all .wmv video files from a specified directory into the given ListBox
+        /// </summary>
+        /// <param name="lstVideos">The ListBox to display the videos</param>
+        /// <param name="folderPath">The path to the directory containing the videos</param>
+        /// <param name="videoPaths">Array of strings containing the file paths</param>
+        /// <param name="selectedIndex">Index of the selected video</param>
         public static void LoadVideos(ListBox lstVideos, string folderPath, out string[] videoPaths, out int selectedIndex)
         {
+            // Get all .wmv files in the specified directory
             videoPaths = System.IO.Directory.GetFiles(folderPath, "*.wmv");
             selectedIndex = 0;
 
+            // If video paths are found, add them to the ListBox
             if (videoPaths != null)
             {
                 foreach (string path in videoPaths)
                 {
+                    // Extract video name from path and add to ListBox
                     string vid = path.Replace(folderPath, string.Empty);
                     vid = vid.Replace(".wmv", string.Empty);
                     lstVideos.Items.Add(vid);
                 }
             }
+            // Set the initial selected index in the ListBox
             lstVideos.SelectedIndex = selectedIndex;
         }
 
+        /// <summary>
+        /// Selects and plays the video from the given ListBox
+        /// </summary>
+        /// <param name="lstVideos">The ListBox containing the video list</param>
+        /// <param name="pnlVideo">The panel where the video will be played</param>
+        /// <param name="pnlSize">The size of the video panel</param>
+        /// <param name="video">Reference to the video object</param>
+        /// <param name="videoPaths">Array of strings containing the file paths</param>
+        /// <param name="lblVideo">The label to display the video name</param>
+        /// <param name="btnPlayPause">The Play/Pause button</param>
+        /// <param name="tmrVideo">Timer for updating the video position</param>
+        /// <param name="nextVideo">Action to play the next video</param>
         public static void SelectVideo(ListBox lstVideos, Panel pnlVideo, Size pnlSize, ref Video video,
                                         string[] videoPaths, System.Windows.Forms.Label lblVideo, 
                                         Button btnPlayPause, Timer tmrVideo, Action nextVideo)
         {
             try
             {
+                // Stop and dispose of the current video if it exists
                 video.Stop();
                 video.Dispose();
             }
             catch { }
 
+            // Get the selected index and play the corresponding video
             int index = lstVideos.SelectedIndex;
             video = new Video(videoPaths[index], false);
             video.Owner = pnlVideo;
             pnlVideo.Size = pnlSize;
             video.Play();
+
+            // Enable the timer for video position updates and set button text to "Pause"
             tmrVideo.Enabled = true;
             btnPlayPause.Text = "Pause";
+
+            // Set up the video ending event to play the next video
             video.Ending += (sender, e) => Video_Ending(nextVideo);
+
+            // Display the selected video name in the label
             lblVideo.Text = lstVideos.Text;
         }
 
+        /// <summary>
+        /// Triggers at the end of a video and plays the next video after a wait time
+        /// </summary>
+        /// <param name="nextVideo">Action to play the next video</param>
         private static void Video_Ending(Action nextVideo)
         {
+            // Wait for 2 seconds and then play the next video
             Task.Factory.StartNew(() =>
             {
                 System.Threading.Thread.Sleep(2000);
@@ -60,28 +110,50 @@ namespace VideoFunctions
             });
         }
 
+        /// <summary>
+        /// Plays the next video from the ListBox
+        /// </summary>
+        /// <param name="lstVideos">The ListBox containing the video list</param>
+        /// <param name="videoCount">The total number of videos</param>
+        /// <param name="setSelectedIndex">Action to set the selected index</param>
         public static void NextVideo(ListBox lstVideos, int videoCount, Action<int> setSelectedIndex)
         {
+            // Increment the index and wrap around if it exceeds the total number of videos
             int index = lstVideos.SelectedIndex;
             index = (index + 1) % videoCount;
             setSelectedIndex(index);
         }
 
+        /// <summary>
+        /// Plays the previous video from the ListBox
+        /// </summary>
+        /// <param name="lstVideos">The ListBox containing the video list</param>
+        /// <param name="videoCount">The total number of videos</param>
+        /// <param name="setSelectedIndex">Action to set the selected index</param>
         public static void PreviousVideo(ListBox lstVideos, int videoCount, Action<int> setSelectedIndex)
         {
+            // Decrement the index and wrap around if it goes below zero
             int index = lstVideos.SelectedIndex;
             index = (index - 1 + videoCount) % videoCount;
             setSelectedIndex(index);
         }
 
+        /// <summary>
+        /// Plays or pauses the current video
+        /// </summary>
+        /// <param name="video">The video object</param>
+        /// <param name="tmrVideo">Timer for updating the video position</param>
+        /// <param name="btnPlayPause">The Play/Pause button</param>
         public static void PlayPauseVideo(Video video, Timer tmrVideo, Button btnPlayPause)
         {
+            // Check if a video is loaded
             if (video == null)
             {
                 MessageBox.Show("No video is currently loaded.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
+            // Play or pause the video based on its current state
             if (!video.Playing)
             {
                 video.Play();
@@ -96,15 +168,30 @@ namespace VideoFunctions
             }
         }
 
+        /// <summary>
+        /// Activates fullscreen mode for video playback
+        /// </summary>
+        /// <param name="form">The main form</param>
+        /// <param name="video">The video object</param>
         public static void ToggleFullscreen(Form form, Video video)
         {
+            // Set form to fullscreen mode and assign the video to the form
             form.FormBorderStyle = FormBorderStyle.None;
             form.WindowState = FormWindowState.Maximized;
             video.Owner = form;
         }
 
+        /// <summary>
+        /// Exits fullscreen mode and returns to the normal window size
+        /// </summary>
+        /// <param name="form">The main form</param>
+        /// <param name="video">The video object</param>
+        /// <param name="formSize">The initial size of the form</param>
+        /// <param name="pnlVideo">The video panel</param>
+        /// <param name="pnlSize">The size of the video panel</param>
         public static void ExitFullscreen(Form form, Video video, Size formSize, Panel pnlVideo, Size pnlSize)
         {
+            // Restore form to its original size and assign the video back to the panel
             form.FormBorderStyle = FormBorderStyle.Sizable;
             form.WindowState = FormWindowState.Normal;
             form.Size = formSize;
@@ -112,18 +199,35 @@ namespace VideoFunctions
             pnlVideo.Size = pnlSize;
         }
 
+        /// <summary>
+        /// Adjusts the volume of the video
+        /// </summary>
+        /// <param name="video">The video object</param>
+        /// <param name="trackVolume">The volume TrackBar</param>
         public static void AdjustVolume(Video video, TrackBar trackVolume)
         {
+            // Set the video volume based on the TrackBar value
             video.Audio.Volume = trackVolume.Value;
         }
 
+        /// <summary>
+        /// Toggles the visibility of the volume TrackBar
+        /// </summary>
+        /// <param name="trackVolume">The volume TrackBar</param>
         public static void ToggleVolumeTrackBar(TrackBar trackVolume)
         {
+            // Toggle the visibility of the volume TrackBar
             trackVolume.Visible = !trackVolume.Visible;
         }
 
+        /// <summary>
+        /// Updates the label displaying the current position and duration of the video
+        /// </summary>
+        /// <param name="video">The video object</param>
+        /// <param name="lblVideoPosition">The label displaying the video position</param>
         public static void UpdateVideoPosition(Video video, System.Windows.Forms.Label lblVideoPosition)
         {
+            // If no video is loaded, display default time
             if (video == null)
             {
                 lblVideoPosition.Text = "00:00:00 / 00:00:00";
@@ -132,9 +236,11 @@ namespace VideoFunctions
 
             try
             {
+                // Get current position and total duration of the video
                 int currentTime = Convert.ToInt32(video.CurrentPosition);
                 int maxTime = Convert.ToInt32(video.Duration);
 
+                // Format and display the video position
                 lblVideoPosition.Text = string.Format("{0:00}:{1:00}:{2:00}", currentTime / 3600, (currentTime / 60) % 60, currentTime % 60)
                                         + " / " +
                                         string.Format("{0:00}:{1:00}:{2:00}", maxTime / 3600, (maxTime / 60) % 60, maxTime % 60);
